c24ecdfd8d475d77414c67ea156c0dd5a084f309
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:42:59 -0300

    Add variáveis nome e sobrenome em aula.js

--- aula.js
+++ aula.js
@@ -14,3 +14,9 @@ console.log('Olá, tudo bem?');
 // criando nossa primeira variavel 
 let mensagem = 'Olá Mundo!'; 
 console.log("esse console é da variável:", mensagem); 
+
+//criar variavel nome e imprimir no console: 
+// criar variavel sobrenome e imprimir no console: 
+let nome = 'Giovanna'; 
+let sobrenome = 'Franco'; 
+console.log('Variáveis nome e sobrenome:', nome, sobrenome); 
