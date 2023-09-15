c42a109384c12306edd7bc34362e51a094b28919
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:39:00 -0300

    alterando arquivo de JavaScript

---
+++ aula.js
@@ -0,0 +1,16 @@
+// Aqui é um alert
+alert('Olá Mundo!!!');
+/*
+Aprendendo Javascript. 
+Mundo legal!
+*/ 
+
+//imprimindo mensagem na tela 
+document.write('Meu nome é Giovanna!');
+
+// Usando o console 
+console.log('Olá, tudo bem?'); 
+
+// criando nossa primeira variavel 
+let mensagem = 'Olá Mundo!'; 
+console.log("esse console é da variável:", mensagem); 
