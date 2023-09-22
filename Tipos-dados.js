595b529d51d7e2be5c3dc3d0dca277074911e83f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 08:50:37 -0300

    Add tipos de dados

---
+++ Tipos-dados.js
@@ -0,0 +1,23 @@
+//cria variável com nº inteiros:
+let idadeAluno = 18; 
+
+console.log(idadeAluno);
+console.log(typeof idadeAluno); //verificar que tipo de variável é. 
+
+//cria uma variável com nº flutuantes (com casas decimais):
+let valorProduto = 30.9;
+console.log(valorProduto);
+
+//cria uma variável string(texto):
+let nomeDoAluno = "Giovanna";   //não pode começar variável com lera maiúscula.
+console.log(nomeDoAluno);
+
+//criar variáveis booleanos, recebem true or false. 
+
+let casado = true;
+console.log(casado); 
+console.log(typeof casado);
+
+// Array - conjunto de elementos
+let linguagens =['Java', 'JavaScript', 'C#']; //sempre o primeiro item do Array é 0.
+console.log(linguagens[1]);
