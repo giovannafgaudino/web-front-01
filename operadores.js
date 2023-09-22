aa42edf1fac27c9b3b9327546b77d39aa0fcb175
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 09:34:40 -0300

    Aprendendo operações matemáticas em js

---
+++ operadores.js
@@ -0,0 +1,19 @@
+let num1 = 10;
+let num2 = 5;
+
+
+//Operações matemáticas com variáveis. 
+////soma: 
+let soma = num1 + num2; //= 15
+//colocar comentário ou texto junto com a variável basta colocar entre aspas. 
+console.log("O resultado entre 10 e 5 é:", soma); //exibir operação na tela. 
+
+////subtração:
+let subtracao = num1 - num2; 
+console.log('O resultado da subtração entre 10 e 5 é:', subtracao);
+////multiplicação:
+let multiplicacao = num1 * num2; 
+console.log('O resultado da multiplicação entre 10 e 5 é:', multiplicacao);
+////divisão:
+let divisao = num1 / num2; 
+console.log('O resultado da divisão entre 10 e 5 é:', divisao);
