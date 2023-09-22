c56a8c53c025d909d4ae0fcc1bc5d52d809a9415
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 09:15:00 -0300

    aprendendo variáveis e comandos

---
+++ texto-numeros.js
@@ -0,0 +1,32 @@
+
+let numFloat = 123.456
+console.log(numFloat);
+//fazer a conversã de um número decimal para inteiro:
+console.log(parseInt(numFloat)); 
+
+//Variável entre aspas = valor de text = string:
+let numString = "123.456"; //variável como texto.
+console.log(parseFloat(numString));//transformando texto em variável número.
+
+//alterar uma variável float para string:
+let num1Float = 145.654;
+let num1String = num1Float.toString();
+let verificar = typeof num1String;
+console.log(verificar);
+
+
+let nomeAluno1 = "Giovanna";
+console.log(nomeAluno1.length); //comando para contar quantas caracteres existem na variável. 
+
+
+let txt = "Estão chegand as provas";
+console.log(txt.indexOf("as")); //verifica o valor da posição do 'termo'
+
+
+let texto = "Estão chegando as provas";
+console.log(texto.lastIndexOf("as")); // verifica o valor da posiçã dos últimos 'termos' (provAS).
+
+
+console.log(txt.slice(0,5)); //vai pegar os termos nas posições e 0 a 5. 'Estão'
+
+console.log(txt.substr(6,8)); // vai pegar os termos dps de determina posição, nesse caso o 6. 
