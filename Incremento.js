af3978640be8fa995f643cc3e2e134edca20592f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 08:34:57 -0300

    Incremento em JS

---
+++ Incremento.js
@@ -0,0 +1,13 @@
+Incremento 
+let valor = 7;
+console.log(valor); //valor igual a 7 
+
+valor = valor + 1;
+console.log(valor); // valor igual a 8 
+
+valor++;  // apenas usado para incrementar apens 1 a variável
+console.log(valor); // valor igual a 9
+
+valor += 5; 
+console.log(valor); // valor igual a 14
+
