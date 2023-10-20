c27e0d65a974d2ca8f7f2f93eff63b779f053436
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 09:49:18 -0300

    Estrutura de repetição 'WHILE'

---
+++ while.js
@@ -0,0 +1,15 @@
+// Estrutura de repetição WHILE 
+
+let  i = 0; 
+while (i <= 5){
+    console.log(i);
+    i++;            // HÁ A NECESSIDADE DE TER O INCREMENTO, SE NÃO FICA EM LOOPING INFINITO 
+}
+
+/* Do while --> quando há a necessidade que mesmo que a condição já esteja atingida ou 
+ou ultrapassada, ee reaize pelo mneos uma vez a instrução. 
+let i = 0 
+do { 
+     console.log(i);
+     i++
+ }while(i==0)*/
