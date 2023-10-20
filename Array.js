2e6d51328eb8fb116c0031366770fd67b2ac82c7
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 09:52:02 -0300

    Arrays

---
+++ Array.js
@@ -0,0 +1,9 @@
+ const times = ['Palmeiras', "Fortaleza", "Botafogo"];
+
+ console.log(times);
+
+ console.table(times);  // transforma em tabela
+
+ times.push("Santos");   //adicionando elementos no array 
+
+ console.table(times);  
