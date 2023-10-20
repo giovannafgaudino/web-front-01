1c2887b7570b2afe23bd47a80fe7cc2550c99f37
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 08:52:18 -0300

    Decremento

---
+++ Decremento.js
@@ -0,0 +1,21 @@
+// Decremento 
+
+let valor = 10; 
+console.log("Valor é 10", valor); 
+
+valor--; 
+console.log("10 - 1 = ", valor); // valor igual a 9 
+
+valor -= 3; 
+console.log("9 - 3 = ", valor); // valor igual a 6 
+
+valor = valor - 1; 
+console.log('6 - ',valor); // valor igual a 5
+
+// Multiplicação
+valor *= 2; 
+console.log(valor); // vai multiplicar o valor atual (5) por 2 (nº definido). 
+
+// Divisão
+valor /= 5; 
+console.log(valor); // vai dividir o valor atua (10) por 5 (nº definido)
