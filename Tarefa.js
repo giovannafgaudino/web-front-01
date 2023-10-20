051957624f2e962f50e969d905b34187aca78bf1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 09:52:24 -0300

    Tarefa de casa

---
+++ Tarefa.js
@@ -0,0 +1,6 @@
+/* 03 - 
+- Abaixo da consante 'cities', delcare uma let "uppercasaCities" e atribua à ela um array vazio; 
+- Adicione no array que a "upperCaseCities" armazena as cidades do array cities com todas as letras maiúsculas,
+utilizanod um loop; 
+- Para ver se deu certo, exiba o uppercaseCities no console. 
+*/
