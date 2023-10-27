02746134cea881418c587ef9b26bf5e70bd1a74d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 08:49:26 -0300

    Aula Function

---
+++ js/dom.js
@@ -0,0 +1,13 @@
+function teste(arg1,arg2) {
+    alert("Olá")
+    console.log(arg1+arg2);
+}
+
+
+
+function imprimeNome(name){
+    console.log(`Meu nome é ${name}`);
+}
+imprimeNome('Giovanna');
+
+teste(7,1);
