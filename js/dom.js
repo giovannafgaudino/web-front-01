59c11475fa297ee37855d8cce8791da6b47c9bb6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 10:00:32 -0300

    Html, CSS, JS e DOM

--- js/dom.js
+++ js/dom.js
@@ -1,4 +1,4 @@
-function teste(arg1,arg2) {
+/*function teste(arg1,arg2) {
     alert("Olá")
     console.log(arg1+arg2);
 }
@@ -11,3 +11,23 @@ function imprimeNome(name){
 imprimeNome('Giovanna');
 
 teste(7,1);
+*/
+
+/*function cliqueAqui(){
+    alert('olá!');      //criando função para evento
+}*/
+
+//Crianda variavel 
+const Sim = document.querySelector(".Sim"); // DOM 
+const Nao = document.querySelector(".Nao");
+const card = document.querySelector(".card");
+
+Sim.addEventListener("click", function(){       /*Criando evento em JS*/
+    alert("Mágica, sumiu!")
+    card.style.display = "none";     /*ao clicar no botão SIM o dispay desaparece*/
+});      
+
+Nao.addEventListener("click", function(){      
+    alert("Mágica, apareceu!")
+    card.style.display = "block";
+});   
