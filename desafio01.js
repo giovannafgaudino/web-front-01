ed7f17aeff77a558785ce1ebcc3f772393579890
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-16 09:49:33 -0300

    Desafios JS

---
+++ desafio01.js
@@ -0,0 +1,11 @@
+/* Uma livraria está fazendo uma promoção para os livros do Harry Potter de R$22 por $R15, isso se levar +7. 
+Faça um programa para ajudar o vendedor a informar o valor final com base no número de livros adquiridos. */
+
+// Pedir o usuário as informações é necessário 
+const prompt = require ('prompt-sync')(); 
+
+
+
+
+
+
---
+++ desafio02.js
@@ -0,0 +1,8 @@
+/*Numa avenida movimentada, o limite de velocidade é de 60km/h. 
+Faça um programa que, com base na velocidade informada, notifique 
+ao guardar o valor da multa (caso haja). A multa é de R$100,00 por km ultrapassado. 
+*/
+
+const prompt = require ('prompt-sync')(); 
+
+ 
